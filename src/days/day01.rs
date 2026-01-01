ac5bf14be2ecb8398536b30c3e36fdfcad5bb0a4
Andre Ribeiro <andresmribeiro@gmail.com>
2026-01-01 22:09:40 +0000

    initial template

---
+++ src/days/day01.rs
@@ -0,0 +1,13 @@
+use crate::{Solution, SolutionPair};
+use std::fs::read_to_string;
+
+///////////////////////////////////////////////////////////////////////////////
+
+pub fn solve() -> SolutionPair {
+    let input = read_to_string();
+    // Your solution here...
+    let sol1: u64 = 0;
+    let sol2: u64 = 0;
+
+    (Solution::from(sol1), Solution::from(sol2))
+}
