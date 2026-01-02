970694a7a4f68b5018ac08f0c073f67f370b73c9
Andre Ribeiro <andresmribeiro@gmail.com>
2026-01-02 01:02:30 +0000

    i hate dis

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -3,39 +3,75 @@ use std::fs::read_to_string;
 
 struct Dial {
     current_val: i32,
+    revolutions: i32,
 }
 
 impl Dial {
     fn new(start_pos: i32) -> Dial {
         return Dial {
             current_val: start_pos,
+            revolutions: 0,
         };
     }
 
-    fn turn(&mut self, turns: i32) {
-        self.current_val = (100 + self.current_val + turns) % 100;
-    }
+    fn turn(&mut self, turns: i32) -> i32 {
+        let start = self.current_val;
 
-    fn peek(&self) -> i32 {
-        return self.current_val;
+        let hits = if turns > 0 {
+            let offset = (100 - start) % 100;
+            let first = if offset == 0 { 100 } else { offset };
+
+            if first > turns {
+                0
+            } else {
+                1 + (turns - first) / 100
+            }
+        } else if turns < 0 {
+            let offset = start % 100;
+            let first = if offset == 0 { 100 } else { offset };
+
+            let t = -turns;
+            if first > t {
+                0
+            } else {
+                1 + (t - first) / 100
+            }
+        } else {
+            0
+        };
+
+        self.current_val = (start + turns).rem_euclid(100);
+        hits
     }
 }
 
 pub fn solve() -> SolutionPair {
     let input = read_to_string("input/day01/input.txt").unwrap();
     let mut dial = Dial::new(50);
-    let mut touched_zero = 0;
+    let mut sol1 = 0;
 
+    // sol 1
     input
         .lines()
         .map(|line| line.replace("L", "-").replace("R", "+"))
         .map(|line| line.parse::<i32>().unwrap())
         .for_each(|turn| {
             dial.turn(turn);
-            if dial.peek() == 0 {
-                touched_zero += 1;
+            if dial.current_val == 0 {
+                sol1 += 1;
             }
         });
 
-    return (Solution::from(touched_zero), Solution::from(0));
+    // sol 2
+    let mut sol2 = 0;
+    dial = Dial::new(50);
+    input
+        .lines()
+        .map(|line| line.replace("L", "-").replace("R", "+"))
+        .map(|line| line.parse::<i32>().unwrap())
+        .for_each(|turn| {
+            sol2 += dial.turn(turn);
+        });
+
+    return (Solution::from(sol1), Solution::from(sol2));
 }
